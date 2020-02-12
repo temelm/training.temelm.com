77ea2d3439f4bb8a2b6792aeee846ed6875b5b8f
Mustafa Temel <mustafatemel90@gmail.com>
2020-02-12 01:48:21 +0000

    Replace switch statement with conditional statements. Add new page info for basket, checkout, and confirmation.

--- src/google-analytics/config.php
+++ src/google-analytics/config.php
@@ -1,19 +1,25 @@
 <?php
-  switch ($_SERVER['SCRIPT_NAME']) {
-    case '/index.php':
-      $current_page = 'Home';
-      $page_title = 'Home';
-      break;
-    case '/catalogue.php':
-      $current_page = 'Catalogue';
-      $page_title = 'Catalogue';
-      break;
-    case '/product.php':
-      $current_page = 'Product';
-      $page_title = 'Product';
-      break;
-    default:
-      $current_page = '404';
-      $page_title = 'Page Not Found :(';
+  $server_script_name = $_SERVER['SCRIPT_NAME'];
+  if (preg_match('/\/index.php/', $server_script_name)) {
+    $current_page = 'Home';
+    $page_title = 'Home';
+  } elseif (preg_match('/\/catalogue.php/', $server_script_name)) {
+    $current_page = 'Catalogue';
+    $page_title = 'Catalogue';
+  } elseif (preg_match('/\/product.php/', $server_script_name)) {
+    $current_page = 'Product';
+    $page_title = 'Product';
+  } elseif (preg_match('/\/basket.php/', $server_script_name)) {
+    $current_page = 'Basket';
+    $page_title = 'Basket';
+  } elseif (preg_match('/\/checkout.php/', $server_script_name)) {
+    $current_page = 'Checkout';
+    $page_title = 'Checkout';
+  } elseif (preg_match('/\/confirmation.php/', $server_script_name)) {
+    $current_page = 'Confirmation';
+    $page_title = 'Confirmation';
+  } else {
+    $current_page = '404';
+    $page_title = 'Page Not Found :(';
   }
 ?>
